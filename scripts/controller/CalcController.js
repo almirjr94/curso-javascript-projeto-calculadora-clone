9089fddf77ad2b6ee6105b4868e6c4054f3c0264
Almir Junior <almirjr944@gmail.com>
2018-09-22 19:04:55 -0300

    botão igual, clicando mais de uma vez

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {
 
+        this._lastNumber = '';
+        this._lastOperator = '';
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -18,6 +20,8 @@ class CalcController {
         setInterval(() => {
             this.setDisplayDateTime();
         }, 1000);
+
+        this.setLastNumberToDisplay();
     }
 
     addEventListenerAll(element, events, fn) {
@@ -28,10 +32,12 @@ class CalcController {
 
     clearAll() {
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     clearEntry() {
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation() {
@@ -54,22 +60,73 @@ class CalcController {
         }
     }
 
+    getResult() {
+        return eval(this._operation.join(""));
+    }
+
     calc() {
-        let last = this._operation.pop();
-        let result = eval(this._operation.join(""));
-        this._operation = [result, last];
+        let last = '';
+        this._lastOperator = this.getLastItem();
+
+        if(this._operation.length < 3){
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator,this._lastNumber];
+
+        }
+
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+            console.log('i');
+
+        }else if (this._operation.length == 3) {
+            console.log('e');
+            this._lastNumber = this.getLastItem(false);
+
+        }
+
+        console.log('_lastOperator', this._lastOperator);
+        console.log('_lastNumber', this._lastNumber);
+
+
+        let result = this.getResult();
+
+        if (last == '%') {
+            result /= 100;
+            this._operation = [result];
+        } else {
+            this._operation = [result];
+
+            if (last) this._operation.push(last);
+        }
+
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay() {
-        let lastNumber;
+    getLastItem(isOperator = true) {
+        let lastItem;
 
         for (let i = this._operation.length - 1; i >= 0; i--) {
-            if (!this.isOperator(this._operation[i])) {
-                lastNumber = this._operation[i];
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i];
                 break;
             }
         }
+
+        if(!lastItem){
+            lastItem = (isOperator) ? this._lastOperator :this._lastNumber
+        }
+
+        return lastItem;
+    }
+
+    setLastNumberToDisplay() {
+        let lastNumber = this.getLastItem(false);
+
+        if (!lastNumber) lastNumber = 0;
+
         this.displayCalc = lastNumber;
     }
 
@@ -113,9 +170,6 @@ class CalcController {
             case 'ce':
                 this.clearEntry();
                 break;
-            case 'porcento':
-                this.clearAll();
-                break;
             case 'divisao':
                 this.addOperation('/');
                 break;
@@ -132,7 +186,7 @@ class CalcController {
                 this.addOperation('%');
                 break;
             case 'igual':
-                this.addOperation('=');
+                this.calc();
                 break;
             case '0':
             case '1':
